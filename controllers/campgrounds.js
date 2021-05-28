8a115a3528caa317c1bebf0b1bc9403871d70a78
Raunak Sarada <raunaksarda1999@gmail.com>
2021-05-28 12:42:53 +0530

    ready to deploy

---
+++ controllers/campgrounds.js
@@ -0,0 +1,80 @@
+const Campground = require('../models/campground');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken});
+const { cloudinary } = require('../cloudinary');
+
+module.exports.index = async (req, res) => {
+    const campground = await Campground.find({}).populate('popupText');//populate will append the whole popup text to a
+    res.render('campgrounds/index', {campground});    //campground because in campground we are storing just the objectId's.
+};
+
+module.exports.renderNewForm = (req, res) => {
+    res.render('campgrounds/new');
+};
+
+module.exports.createCampground = async(req, res) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,   //used to set the location in a map.
+        limit: 1
+    }).send()
+    const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));//used to set multiple images in campground.
+    campground.author = req.user._id;                       //we are storing the userId coming through passport(from sessionId).
+    await campground.save();
+    req.flash('success', 'Successfully added a new band!');
+    res.redirect(`campgrounds/${campground._id}`);
+};
+
+module.exports.showCampground = async(req, res) => {
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    if(!campground) {
+        req.flash('error', 'Cannot find that Band!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', {campground});
+};
+
+module.exports.renderEditForm = async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    if(!campground) {
+        req.flash('error', 'Cannot find that Band!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/edit', { campground });
+};
+
+module.exports.updateCampground = async(req, res) => {
+    const { id } = req.params;
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });//req.body.campground returns an object and 
+    campground.geometry = geoData.body.features[0].geometry;               //'...' is used to spread the properties of the object.
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
+    if(req.body.deleteImages) {
+        for(let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: {images: {filename: { $in: req.body.deleteImages } } } } );
+    }
+    req.flash('success', 'Successfully updated band!');
+    res.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.deleteCampground = async(req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted Band');
+    res.redirect('/campgrounds');
+};
+
